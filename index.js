53e89dd39070e8c77933c43c85b0df5d936ec244
Vu Nguyen <nguyenbatuanvu@gmail.com>
2019-03-25 07:17:20 +0700

    save changes

--- index.js
+++ index.js
@@ -4,29 +4,20 @@ var io = require('socket.io')(server);
 var bodyParser = require('body-parser');
 var kafkaProducer = require('./kafka');
 
-
-// try create a kafka topic
-// const topic = {
-//   topic: 'topic1',
-//   partitions: 1,
-//   replicationFactor: 2
-// };
-// kafkaProducer.createTopic(topic);
-
-
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.post('/push_network_stats',function(req,res){
-  const statsData = JSON.stringify(req.body.data);
+  const statsData = JSON.stringify(req.body);
   kafkaProducer.produceNetworkStats(statsData);
   res.json();
 });
 
 
 io.on('connection', function (socket) {
-  socket.on('topic1', (data) => {
+  socket.on('NetworkStats', (data) => {
+    kafkaProducer.produceNetworkStats(data);
   })
 });
 
--- kafka.js
+++ kafka.js
@@ -2,15 +2,13 @@ var kafka = require('kafka-node');
 var Producer = kafka.Producer;
 
 
-class OhmniKafkaProducer {
+class MyKafkaProducer {
   constructor() {
     this._client = new kafka.KafkaClient();
     this._producer = new Producer(this._client);
 
-    this._ready = false;
     this._producer.on('ready', function () {
       console.log('Producer is ready');
-      this._ready = true;
     });
 
     this._producer.on('error', function (err) {
@@ -18,23 +16,8 @@ class OhmniKafkaProducer {
       console.log(err);
     });
 
-    this.createTopic = this.createTopic.bind(this);
     this.produceNetworkStats = this.produceNetworkStats.bind(this);
   }
-
-  get ready(){
-    return this._ready;
-  }
-
-  createTopic(topic) {
-    this._producer.createTopics([topic],false,function (err, data) {
-      if (err) {
-        console.log("error create kafka topics", err);
-      } else {
-        console.log("create kafka topics successfully", data);
-      }
-    });
-  };
 
   produceNetworkStats(statsData) {
     const topic = "NetworkStats";
@@ -48,6 +31,6 @@ class OhmniKafkaProducer {
 
 }
 
-const ohmniKafkaProducer = new OhmniKafkaProducer();
+const myKafkaProducer = new MyKafkaProducer();
 
-module.exports = ohmniKafkaProducer;
+module.exports = myKafkaProducer;
